a1468c32e7996de62ac47baafecb26b256247166
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-10-11 20:48:48 +0200

    Add files via upload

---
+++ ZadatakZaVjezbu5/ZadatakZaVjezbu5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaVjezbu5
+{
+    internal class Program
+    {
+
+        class Search
+        {
+            public int BinarySearch(int[] array, int x)
+            {
+                return Array.BinarySearch(array, x);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Search search = new Search();
+
+            int[] array = new int[100];
+            int s = 1;
+            int r = 75;
+
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = s;
+                s = s + 2;
+            }
+
+            Console.WriteLine(search.BinarySearch(array, r));
+            Console.WriteLine(array[37]);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ZadatakZaVjezbu5/ZadatakZaVjezbu5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZadatakZaVjezbu5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZadatakZaVjezbu5")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b6186cfc-e653-4d49-8f27-109e352b0369")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
